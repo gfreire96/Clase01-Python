81782902458f435eb4b7387209b57c4b78d71471
Gabriel Freire Parola <gfreire96@gmail.com>
2025-08-13 17:35:08 -0300

    Actualizo logger_base.py

--- logger_base.py
+++ logger_base.py
@@ -1,12 +1,13 @@
-import logging as log
+import logging
 
-# docs.python.org/3/howto/logging.html
-# Llamamos una configuracion basica
+class LoggerBase:
+    @staticmethod
+    def configurar():
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s [%(levelname)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        return logging.getLogger()
 
-log.basicConfig(level=log.DEBUG,
-                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
-                datefmt='%I:%M:%S %p',
-                handlers=[
-                    log.FileHandler('capa_datos.log'),
-                    log.StreamHandler()
-                ])
+log = LoggerBase.configurar()
